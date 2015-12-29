b2310ed8d63aeb4b5ea5484f4b77d115df98fe14
xiangshengdong <xiangshengdong@yunba.io>
2015-12-29 15:26:48 +0800

    Add a test file: publish.sh

---
+++ test/publish.sh
@@ -0,0 +1,11 @@
+APPKEY="563c4afef085fc471efdf803"
+ALIAS="pi_house_shdxiang"
+SECKEY="sec-zxhrt0bbwTHkRBsj8b66VL1dbQ52IFKdkfnZzdI6Qli0zPIx"
+
+data="{\"method\":\"publish_to_alias\", \"appkey\":\"${APPKEY}\", \"seckey\":\"${SECKEY}\", \"alias\":\"${ALIAS}\", \"msg\":\"${1}\"}"
+
+echo ${data}
+curl -l -H "Content-type: application/json" -X POST -d "${data}" http://rest.yunba.io:8080
+
+echo
+

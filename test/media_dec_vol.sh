0cf1c5391cf16ff463e963b4a400e306fe8fc5a4
shdxiang <shdxiang@qq.com>
2016-01-14 18:01:56 +0800

    Add volume control test

---
+++ test/media_dec_vol.sh
@@ -0,0 +1,5 @@
+msg="{\\\"act\\\":\\\"media_dec_vol\\\"}"
+
+#echo "${msg}"
+./publish.sh "${msg}"
+
---
+++ test/media_inc_vol.sh
@@ -0,0 +1,5 @@
+msg="{\\\"act\\\":\\\"media_inc_vol\\\"}"
+
+#echo "${msg}"
+./publish.sh "${msg}"
+
